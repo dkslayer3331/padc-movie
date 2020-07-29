b3d91c082b3614f11df2ce4f6e18a13bf5000f19
moehtet <laxus3331@gmail.com>
2020-07-29 15:56:55 +0630

    base recycler and viewholder

--- app/src/main/java/com/mhst/padc_movie_app/MainActivity.kt
+++ app/src/main/java/com/mhst/padc_movie_app/MainActivity.kt
@@ -3,8 +3,8 @@ package com.mhst.padc_movie_app
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.CalendarContract
 import com.mhst.padc_movie_app.adapters.SliderAdapter
+import com.mhst.padc_movie_app.utils.sliderUrlList
 import com.smarteist.autoimageslider.SliderAnimations
 import kotlinx.android.synthetic.main.activity_main.*
 
---
+++ app/src/main/java/com/mhst/padc_movie_app/adapters/BaseRecyclerAdapter.kt
@@ -0,0 +1,28 @@
+package com.mhst.padc_movie_app.adapters
+
+import androidx.recyclerview.widget.RecyclerView
+import com.mhst.padc_movie_app.views.viewholders.BaseViewHolder
+
+abstract class BaseRecyclerAdapter<T : BaseViewHolder<W>,W>
+    : RecyclerView.Adapter<T>() {
+
+    var mData : MutableList<W> = arrayListOf()
+
+    override fun getItemCount(): Int {
+        return mData.count()
+    }
+
+    override fun onBindViewHolder(holder: T, position: Int) {
+        holder.bindData(mData[position])
+    }
+
+    fun setNewData(data : MutableList<W>){
+        mData = data
+        notifyDataSetChanged()
+    }
+
+    fun appendNewData(data : List<W>){
+        mData.addAll(data)
+        notifyDataSetChanged()
+    }
+}
