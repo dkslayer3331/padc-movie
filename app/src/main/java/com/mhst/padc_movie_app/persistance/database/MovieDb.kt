5684c6fee36450d89b01fb8cd47f0554d3f007a8
moehtet <laxus3331@gmail.com>
2020-08-01 12:19:52 +0630

    geg

--- app/src/main/java/com/mhst/padc_movie_app/persistance/database/MovieDb.kt
+++ app/src/main/java/com/mhst/padc_movie_app/persistance/database/MovieDb.kt
@@ -4,13 +4,14 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.mhst.padc_movie_app.data.vos.GenreVo
 import com.mhst.padc_movie_app.data.vos.MovieVO
 import com.mhst.padc_movie_app.data.vos.PersonVO
 import com.mhst.padc_movie_app.persistance.daos.GenreDao
 import com.mhst.padc_movie_app.persistance.daos.MovieDao
 import com.mhst.padc_movie_app.persistance.daos.PersonDao
 
-@Database(entities = [MovieVO::class,PersonVO::class], version = 1, exportSchema = false)
+@Database(entities = [MovieVO::class,PersonVO::class,GenreVo::class], version = 2, exportSchema = false)
     abstract class MovieDb : RoomDatabase() {
         companion object {
             val DB_NAME = "PADC_MOVIES.DB"
